ef9dd641c8356d2fa1ca4bee5ea076dd90ca2b84
Gulzarema Mardenova <mardenova@usf.edu>
2020-02-07 12:06:54 -0500

    Latest update

---
+++ OOP In Class/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_In_Class
+{
+    class Car
+    {
+        public string color;
+        public string model;
+        public int maxSpeed;
+        public int year;
+        public void fullThrottle()    // method
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+    }
+}
---
+++ OOP In Class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP_In_Class
+{
+    class Program
+    {
+        class Car
+        {
+            public string model;
+            public string color;
+            public int year;
+            public Car(string modelName, string modelColor, int modelYear)
+            {
+                model = modelName;
+                color = modelColor;
+                year = modelYear;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "Red", 1969);
+            Console.WriteLine(Ford.model + ", " + Ford.color + ", " + Ford.year);
+        }
+    }
+}
